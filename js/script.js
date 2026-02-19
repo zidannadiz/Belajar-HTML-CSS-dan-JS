52ad6e91b409831de0b30a2c28d6d4164528c7e1
Muhamad Zidan <zidanlangoet10987@gmail.com>
2026-02-19 19:07:37 +0700

    DIY

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const a = 100;
+
+if (a > 100) {
+  console.log("Kamu Kamu Eaaaaa");
+} else if (a < 100) {
+  console.log("Bwahahahaha");
+} else {
+  console.log("Kamu Kamu Eaaaaa");
+}
+
+function sapa(orang) {
+  console.log("Hai", orang);
+}
+
+sapa("Zidan");
